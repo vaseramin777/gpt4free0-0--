582e83373e5aefbf12c516df818243813f910421
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 17:34:37 +0300

    comment

--- etc/testing/log_time.py
+++ etc/testing/log_time.py
@@ -2,20 +2,32 @@ from time import time
 
 
 async def log_time_async(method: callable, **kwargs):
-    start = time()
-    result = await method(**kwargs)
-    secs = f"{round(time() - start, 2)} secs"
-    return " ".join([result, secs]) if result else secs
+    # This function logs the time taken to execute an asynchronous function.
+    # It takes a method (a callable object) and any number of keyword arguments
+    # (kwargs) to be passed to the method.
+
+    start = time()  # Record the start time before calling the method.
+    result = await method(**kwargs)  # Call the method asynchronously.
+    secs = f"{round(time() - start, 2)} secs"  # Calculate the time taken.
+    return " ".join([result, secs]) if result else secs  # Return the result and time taken.
 
 
 def log_time_yield(method: callable, **kwargs):
-    start = time()
-    result = yield from method(**kwargs)
-    yield f" {round(time() - start, 2)} secs"
+    # This function logs the time taken to execute a generator function.
+    # It takes a method (a callable object) and any number of keyword arguments
+    # (kwargs) to be passed to the method.
+
+    start = time()  # Record the start time before calling the method.
+    result = yield from method(**kwargs)  # Call the method and get the generator result.
+    yield f" {round(time() - start, 2)} secs"  # Yield the time taken.
 
 
 def log_time(method: callable, **kwargs):
-    start = time()
-    result = method(**kwargs)
-    secs = f"{round(time() - start, 2)} secs"
-    return " ".join([result, secs]) if result else secs
+    # This function logs the time taken to execute a synchronous function.
+    # It takes a method (a callable object) and any number of keyword arguments
+    # (kwargs) to be passed to the method.
+
+    start = time()  # Record the start time before calling the method.
+    result = method(**kwargs)  # Call the method synchronously.
+    secs = f"{round(time() - start, 2)} secs"  # Calculate the time taken.
+    return " ".join([result, secs]) if result else secs  # Return the result and time taken.
