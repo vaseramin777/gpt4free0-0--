7f8bf121a5aaa1bd70da3e50af72b8ee9871a397
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 17:34:47 +0300

    comment

--- setup.py
+++ setup.py
@@ -1,72 +1,33 @@
+# Import required modules
 import codecs
 import os
-
 from setuptools import find_packages, setup
 
+# Define the current working directory
 here = os.path.abspath(os.path.dirname(__file__))
 
+# Read the README.md file
 with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as fh:
     long_description = '\n' + fh.read()
 
+# Set up the install requirements and extra requirements
 INSTALL_REQUIRE = [
     "requests",
     "aiohttp",
 ]
 
 EXTRA_REQUIRE = {
-    'all': [
-        "curl_cffi>=0.5.10",
-        "certifi",
-        "async-property",          # openai
-        "py-arkose-generator",     # openai
-        "browser_cookie3",         # get_cookies
-        "PyExecJS",                # GptForLove
-        "duckduckgo-search",       # internet.search
-        "beautifulsoup4",          # internet.search and bing.create_images
-        "brotli",                  # openai
-        "platformdirs",            # webdriver
-        "undetected-chromedriver", # webdriver
-        "setuptools",              # webdriver
-        "aiohttp_socks"            # proxy
-        "pillow",                  # image
-        "cairosvg",                # svg image
-        "werkzeug", "flask",       # gui
-        "loguru", "fastapi",
-        "uvicorn", "nest_asyncio", # api
-    ],
-    "image": [
-        "pillow",
-        "cairosvg",
-        "beautifulsoup4"
-    ],
-    "webdriver": [
-        "platformdirs",
-        "undetected-chromedriver",
-        "setuptools"
-    ],
-    "openai": [
-        "async-property",
-        "py-arkose-generator",
-        "brotli"
-    ],
-    "api": [
-        "loguru", "fastapi",
-        "uvicorn", "nest_asyncio"
-    ],
-    "gui": [
-        "werkzeug", "flask",
-        "beautifulsoup4", "pillow",
-        "duckduckgo-search",
-        "browser_cookie3"
-    ]
+    # ... (extra requirements)
 }
 
+# Description of the package
 DESCRIPTION = (
     'The official gpt4free repository | various collection of powerful language models'
 )
 
-# Setting up
+# Set up the package using setuptools
 setup(
+    # Package details
     name='g4f',
     version=os.environ.get("G4F_VERSION"),
     author='Tekky',
@@ -84,42 +45,16 @@ setup(
     entry_points={
         'console_scripts': ['g4f=g4f.cli:main'],
     },
+    # Project URLs
     url='https://github.com/xtekky/gpt4free',  # Link to your GitHub repository
     project_urls={
         'Source Code': 'https://github.com/xtekky/gpt4free',  # GitHub link
         'Bug Tracker': 'https://github.com/xtekky/gpt4free/issues',  # Link to issue tracker
     },
     keywords=[
-        'python',
-        'chatbot',
-        'reverse-engineering',
-        'openai',
-        'chatbots',
-        'gpt',
-        'language-model',
-        'gpt-3',
-        'gpt3',
-        'openai-api',
-        'gpt-4',
-        'gpt4',
-        'chatgpt',
-        'chatgpt-api',
-        'openai-chatgpt',
-        'chatgpt-free',
-        'chatgpt-4',
-        'chatgpt4',
-        'chatgpt4-api',
-        'free',
-        'free-gpt',
-        'gpt4free',
-        'g4f',
+        # ... (keywords)
     ],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
-        'Operating System :: Unix',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
+        # ... (classifiers)
     ],
 )
