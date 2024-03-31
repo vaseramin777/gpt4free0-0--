a6e4dc6525f474eaabf4e537a7d073fee58d1593
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 17:35:38 +0300

    comment

--- g4f/debug.py
+++ g4f/debug.py
@@ -1,5 +1,5 @@
-from .base_provider import ProviderType
+from .base_provider import ProviderType  # Importing the ProviderType class from the base_provider module.
 
-logging: bool = False
-version_check: bool = True
-last_provider: ProviderType = None
+logging: bool = False  # Setting a flag to control whether logging is enabled or not (default is False).
+version_check: bool = True  # Setting a flag to control whether to perform a version check (default is True).
+last_provider: ProviderType = None  # Initializing a variable to store the last provider used (default is None).
