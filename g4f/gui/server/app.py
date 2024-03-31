bc84aeb833d9e27b3492d62d08359687e0525099
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 17:34:54 +0300

    comment

--- g4f/gui/server/app.py
+++ g4f/gui/server/app.py
@@ -1,3 +1,4 @@
+# Import the Flask module and create a new Flask web server instance
+# '__name__' is the name of the current module, and 'template_folder' is the directory where Flask should look for static HTML templates
 from flask import Flask
-
 app = Flask(__name__, template_folder='./../client/html')
