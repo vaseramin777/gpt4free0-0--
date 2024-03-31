e958d5b6c9f3aff9084779248636a59f85ed08b1
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 17:35:31 +0300

    comment

--- g4f/Provider/deprecated/Wuguokai.py
+++ g4f/Provider/deprecated/Wuguokai.py
@@ -1,25 +1,35 @@
 from __future__ import annotations
 
 import random
-
 import requests
-
 from ...typing import Any, CreateResult
 from ..base_provider import AbstractProvider, format_prompt
 
 
 class Wuguokai(AbstractProvider):
+    """
+    Wuguokai is a subclass of AbstractProvider, providing a specific implementation for the Wuguokai API.
+    """
     url = 'https://chat.wuguokai.xyz'
     supports_gpt_35_turbo = True
     working = False
 
     @staticmethod
     def create_completion(
-        model: str,
-        messages: list[dict[str, str]],
-        stream: bool,
-        **kwargs: Any,
+            model: str,
+            messages: list[dict[str, str]],
+            stream: bool,
+            **kwargs: Any,
     ) -> CreateResult:
+        """
+        create_completion is a static method that sends a POST request to the Wuguokai API and returns the response.
+
+        :param model: The model to be used for the completion.
+        :param messages: A list of dictionaries containing messages for the completion.
+        :param stream: A boolean indicating if the response should be streamed.
+        :param kwargs: Additional keyword arguments.
+        :return: A CreateResult object.
+        """
         headers = {
             'authority': 'ai-api.wuguokai.xyz',
             'accept': 'application/json, text/plain, */*',
@@ -35,12 +45,14 @@ class Wuguokai(AbstractProvider):
             'sec-fetch-site': 'same-site',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
         }
-        data ={
+
+        data = {
             "prompt": format_prompt(messages),
             "options": {},
             "userId": f"#/chat/{random.randint(1,99999999)}",
             "usingContext": True
         }
+
         response = requests.post(
             "https://ai-api20.wuguokai.xyz/api/chat-process",
             headers=headers,
@@ -48,10 +60,7 @@ class Wuguokai(AbstractProvider):
             json=data,
             proxies=kwargs.get('proxy', {}),
         )
+
+        # Split the response text and handle errors
         _split = response.text.split("> 若回答失败请重试或多刷新几次界面后重试")
-        if response.status_code != 200:
-            raise Exception(f"Error: {response.status_code} {response.reason}")
-        if len(_split) > 1:
-            yield _split[1].strip()
-        else:
-            yield _split[0].strip()
+        if response.status_code != 
