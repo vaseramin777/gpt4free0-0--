0c3f396e0b6cf049f1761ad122d565f4946478c7
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 17:34:37 +0300

    comment

--- etc/unittest/__main__.py
+++ etc/unittest/__main__.py
@@ -1,7 +1,14 @@
 import unittest
+
+# Importing the unittest module for running tests
+
 from .asyncio import *
 from .backend import *
 from .main import *
 from .model import *
 
+# Importing necessary modules and sub-modules for testing
+
 unittest.main()
+
+# Running the unittest.main() function to execute all the test cases
